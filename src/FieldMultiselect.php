de2718f613b54fb495fe7d5b7386aca18c4da076
Charlie Page <charliepage88@gmail.com>
2020-06-23 19:11:48 -0400

    sync

--- src/FieldMultiselect.php
+++ src/FieldMultiselect.php
@@ -2,7 +2,17 @@
 
 namespace Adaptcms\FieldMultiselect;
 
+use Adaptcms\Base\Models\Package;
+
 class FieldMultiselect
 {
-    // Build wonderful things
+  /**
+  * On Install
+  *
+  * @return void
+  */
+  public function onInstall()
+  {
+    Package::syncPackageFolder(get_class());
+  }
 }
