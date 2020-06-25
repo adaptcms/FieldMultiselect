019f3ff2bd3b2da716af224736dbd77c6b1094a4
Charlie Page <charliepage88@gmail.com>
2020-06-25 17:58:22 -0400

    sp update

--- src/FieldMultiselectServiceProvider.php
+++ src/FieldMultiselectServiceProvider.php
@@ -66,7 +66,7 @@ class FieldMultiselectServiceProvider extends ServiceProvider
     $commands = [];
     foreach (glob(__DIR__ . '/Console/Commands/*.php') as $row) {
       // init class path
-      $classPath = '\\' . $vendorName . '\\' . $packageName . '\\Console\\Commands\\';
+      $classPath = '\\Adaptcms\\FieldMultiselect\\Console\\Commands\\';
 
       // class path with command file class name
       $commandFileClass = str_replace('.php', '', basename($row));
